4456c40307382bea4562900cde2af4621edfd20f
DerouillonAlice <pro@alicedr.com>
2026-02-13 11:12:31 +0100

    feat: update composer, wordpress version

--- web/app/themes/zetruc-theme/vite.config.js
+++ web/app/themes/zetruc-theme/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin'
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin'
 
 export default defineConfig({
-  base: '/app/themes/zetruc-theme/public/build/',
+  base: '/app/themes/zetructheme-/public/build/',
   plugins: [
     tailwindcss(),
     laravel({
