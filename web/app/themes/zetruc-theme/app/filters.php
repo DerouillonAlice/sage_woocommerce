375fe0fb56fb486f52b5f37046ab8cbab01b1d98
DerouillonAlice <pro@alicedr.com>
2026-02-24 08:35:46 +0100

    feat: update style css

--- web/app/themes/zetruc-theme/app/filters.php
+++ web/app/themes/zetruc-theme/app/filters.php
@@ -20,3 +20,34 @@ add_filter('sage-woocommerce/templates', function ($paths) {
     return $paths;
 });
 
+
+/**
+ * Désactiver les styles globaux WordPress injectés automatiquement.
+ * Ils seront réenregistrés dans un layer CSS personnalisé.
+ *
+ * @return void
+ */
+add_action('wp_enqueue_scripts', function () {
+    wp_dequeue_style('global-styles');
+    wp_deregister_style('global-styles');
+}, 10);
+
+/**
+ * Réenregistrer les styles globaux WordPress dans un layer CSS.
+ * Cela permet à Tailwind de reprendre correctement la priorité.
+ *
+ * @return void
+ */
+add_action('wp_enqueue_scripts', function () {
+    $stylesheet = wp_get_global_stylesheet();
+    
+    if (empty($stylesheet)) {
+        return;
+    }
+
+    wp_register_style('global-styles', false);
+    wp_add_inline_style('global-styles', "@layer global-block-styles { $stylesheet }");
+    wp_enqueue_style('global-styles');
+
+    wp_add_global_styles_for_blocks();
+}, 11);
